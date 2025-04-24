21b6f5314150a70e0efa340d62fc5639933f29fd
Guilherme Ali <guilherme.ali@hotmail.com>
2025-04-23 22:41:58 -0300

    Utilização da biblioteca bolderflight para mais rapido acesso aos estados do drone

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,16 @@
 #define STATE_SIZE 6
 #define CONTROL_SIZE 3
 
+unsigned long max_exectuion_time = 0; // Tempo máximo de execução em microssegundos
 
-void print_roll_pitch_yaw();
-void updateSystemMatrix(float p_angle, float q_angle, float r_angle);
+void updateSystemMatrix(float p, float q, float r);
+void printGains();
+void displayIMU();
 
 const float Ixx = 0.00184;  // Momento de inércia em torno do eixo x
 const float Iyy = 0.00225;  // Momento de inércia em torno do eixo y   
 const float Izz = 0.00338;  // Momento de inércia em torno do eixo z
-const float Ir = 0.0001;   // Momento de inércia do conjunto do motor e hélice
+const float Ir = 0.00001;   // Momento de inércia do conjunto do motor e hélice
 float omega_r = 2500; // Velocidade angular do motor
 
 float Ad[STATE_SIZE * STATE_SIZE];
@@ -55,20 +57,29 @@ float R[CONTROL_SIZE * CONTROL_SIZE] = {
 // Controlador e variáveis de estado
 AutoLQR controller(STATE_SIZE, CONTROL_SIZE);
 
-// Inicializa o MPU9250
-MPU9250 mpu;
+MPU9250 IMU(SPI, 5);
+int status;
 
 void setup()
 {
     Serial.begin(115200);
-    Wire.begin();
-    delay(2000);
 
-    if (!mpu.setup(0x68)) {  // change to your own address
-        while (1) {
-            Serial.println("Falha na conexão do MPU. Por favor, verifique sua conexão.");
-        }
+    SPI.begin();
+    status = IMU.begin();
+    if (status < 0) {
+        Serial.println("IMU initialization unsuccessful");
+        Serial.println("Check IMU wiring or try cycling power");
+        Serial.print("Status: ");
+        Serial.println(status);
+        while(1) {}
     }
+
+    // setting the accelerometer full scale range to +/-8G 
+    IMU.setAccelRange(MPU9250::ACCEL_RANGE_2G);
+    // setting the gyroscope full scale range to +/-500 deg/s
+    IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
+    // setting DLPF bandwidth to 20 Hz
+    IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_92HZ);
 
     // Parâmetros para discretização
     float samplingTime = 0.001; // Tempo de amostragem em segundos
@@ -89,73 +100,56 @@ void loop(){
 
     unsigned long startTime = micros(); // Captura o tempo inicial
 
-    if (mpu.update()) {
-        static uint32_t prev_ms = micros();
-        if (micros() > prev_ms + 30000) { 
-            print_roll_pitch_yaw();
+    IMU.readSensor();
 
-            // Atualiza a matriz do sistema com os valores atuais
-            float p = mpu.getGyroX() * DEG_TO_RAD; // Converte de graus para radianos
-            float q = mpu.getGyroY() * DEG_TO_RAD; // Converte de graus para radianos
-            float r = mpu.getGyroZ() * DEG_TO_RAD; // Converte de graus para radianos
-            updateSystemMatrix(p, q, r);
-            prev_ms = micros();
-        }
-    }
+    // Atualiza a matriz do sistema com os valores atuais
+    float p = IMU.getGyroX_rads();
+    float q = IMU.getGyroY_rads(); 
+    float r = IMU.getGyroZ_rads(); 
+    updateSystemMatrix(p, q, r);  
 
     // Calcula os ganhos ótimos
-    if (controller.computeGains()) {
-        
+    controller.computeGains();
 
-        unsigned long endTime = micros(); // Captura o tempo final
-        unsigned long executionTime = endTime - startTime; // Calcula o tempo decorrido
-        
-        Serial.print("Tempo de computação do LQR: ");
-        Serial.print(executionTime);
-        Serial.println(" us");
-        
-        Serial.println("Ganhos do LQR calculados com sucesso");
+    unsigned long endTime = micros(); // Captura o tempo final
+    unsigned long executionTime = endTime - startTime; // Calcula o tempo decorrido
 
-        // Exporta os ganhos calculados
-        float exportedGains[CONTROL_SIZE * STATE_SIZE];
-        controller.exportGains(exportedGains);
-        Serial.println("Ganhos Exportados:");
-        for (int i = 0; i < CONTROL_SIZE; i++) {
-            for (int j = 0; j < STATE_SIZE; j++) {
-                Serial.print(exportedGains[i * STATE_SIZE + j], 6);
-                Serial.print(" ");
-            }
-            Serial.println();
-        }
-    } 
-    //delay(1000); 
+
+    if (executionTime > max_exectuion_time & executionTime < 500000) {
+        max_exectuion_time = executionTime;
+    }
+    
+    
+    Serial.print("Tempo_execucao:");
+    Serial.println(executionTime);
+    Serial.print(",");
+    Serial.print("Tempo_Maximo:");
+    Serial.println(max_exectuion_time);
+
+    /*
+    printGains();
+        
+    displayIMU();
+    */
+
 
 }
 
-void print_roll_pitch_yaw() {
-    Serial.print("Yaw, Pitch, Roll: ");
-    Serial.print(mpu.getEulerX(), 2);
-    Serial.print(", ");
-    Serial.print(mpu.getEulerY(), 2);
-    Serial.print(", ");
-    Serial.println(mpu.getEulerZ(), 2);
-}
-
-void updateSystemMatrix(float p_angle, float q_angle, float r_angle) {
+void updateSystemMatrix(float p, float q, float r) {
     // Update the continuous-time A matrix with current angular velocities
     // The first 3 rows remain constant
     
     // Row 4 (index 3)
-    A[3 * STATE_SIZE + 4] = ((Iyy - Izz) / (2 * Ixx)) * r_angle - Ir * omega_r / Ixx;
-    A[3 * STATE_SIZE + 5] = ((Iyy - Izz) / (2 * Ixx)) * q_angle;
+    A[3 * STATE_SIZE + 4] = ((Iyy - Izz) / (2 * Ixx)) * r - Ir * omega_r / Ixx;
+    A[3 * STATE_SIZE + 5] = ((Iyy - Izz) / (2 * Ixx)) * q;
     
     // Row 5 (index 4)
-    A[4 * STATE_SIZE + 3] = ((Izz - Ixx) / (2 * Iyy)) * r_angle - Ir * omega_r / Iyy;
-    A[4 * STATE_SIZE + 5] = ((Izz - Ixx) / (2 * Iyy)) * p_angle;
+    A[4 * STATE_SIZE + 3] = ((Izz - Ixx) / (2 * Iyy)) * r - Ir * omega_r / Iyy;
+    A[4 * STATE_SIZE + 5] = ((Izz - Ixx) / (2 * Iyy)) * p;
     
     // Row 6 (index 5)
-    A[5 * STATE_SIZE + 3] = ((Ixx - Iyy) / (2 * Izz)) * q_angle;
-    A[5 * STATE_SIZE + 4] = ((Ixx - Iyy) / (2 * Izz)) * p_angle;
+    A[5 * STATE_SIZE + 3] = ((Ixx - Iyy) / (2 * Izz)) * q;
+    A[5 * STATE_SIZE + 4] = ((Ixx - Iyy) / (2 * Izz)) * p;
     
     // Discretize the updated A matrix
     float samplingTime = 0.001; // Same sampling time as in setup()
@@ -172,4 +166,42 @@ void updateSystemMatrix(float p_angle, float q_angle, float r_angle) {
     
     // Update the controller with the new state matrix
     controller.setStateMatrix(Ad);
+}
+
+void printGains(){
+    Serial.println("Ganhos do LQR calculados com sucesso");
+
+    // Exporta os ganhos calculados
+    float exportedGains[CONTROL_SIZE * STATE_SIZE];
+    controller.exportGains(exportedGains);
+    Serial.println("Ganhos Exportados:");
+    for (int i = 0; i < CONTROL_SIZE; i++) {
+        for (int j = 0; j < STATE_SIZE; j++) {
+            Serial.print(exportedGains[i * STATE_SIZE + j], 6);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
+}
+
+void displayIMU() {
+    Serial.print(IMU.getAccelX_mss(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getAccelY_mss(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getAccelZ_mss(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroX_rads(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroY_rads(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroZ_rads(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagX_uT(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagY_uT(), 6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagZ_uT(), 6);
+    Serial.print("\t");
+    Serial.println(IMU.getTemperature_C(), 6);
 }
